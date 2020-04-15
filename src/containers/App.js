f8a95aadd98f8b6fc4c0e8cddfce6ef0b984b588
Alyssa bartuch <alyssabartuch@Alyssas-MacBook-Pro.local>
2020-04-15 09:33:00 -0500

    trying bug fix

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,10 +17,7 @@ class App extends Component {
     }
 
     componentDidMount() {
-        let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-        let targetUrl = 'https://jsonplaceholder.typicode.com/users';
-
-        fetch(proxyUrl + targetUrl)
+        fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => this.setState({ kittens: users, isLoading: false }));
         // console.log('component mounted');
@@ -33,7 +30,9 @@ class App extends Component {
     render() {
         // console.log('render');
         const { kittens, searchField, isLoading } = this.state;
-        
+        const filteredKittens = kittens.filter(kitten => {
+            return kitten.name.toLowerCase().includes(searchField.toLowerCase());
+        });
 
         // return (isLoading) ?
         // <h1 className="tc">Loading...</h1> : (
@@ -48,16 +47,9 @@ class App extends Component {
         //     </div>
         // );
 
-        if (!kittens.length || isLoading) { 
-            console.log(kittens);
-            
+        if (isLoading) { 
             return <h1 className="tc">Loading...</h1> 
         } else {
-            console.log("show");
-            const filteredKittens = kittens.filter(kitten => {
-                return kitten.name.toLowerCase().includes(searchField.toLowerCase());
-            });
-
             return (
                 <div className="tc">
                 <h1 id="heading">Kitten Friends</h1>
